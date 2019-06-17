d05d2115c40566a9beda6bed075140050bf00c42
emanuele <emanuele.biotti@gmail.com>
2019-06-17 17:56:58 +0200

    impostazione esercizio

---
+++ data.php
@@ -0,0 +1,41 @@
+<?php
+
+function creaCodice(){
+
+  $playercode = rand(1,999);
+  return $playercode;
+}
+
+function generaRandom() {
+  $randomNumber = rand(1,20);
+  return $randomNumber;
+}
+
+function generaLettere() {
+  $length = 3;
+
+  $randomletter = substr(str_shuffle("abcdefghijklmnopqrstuvwxyz"), 0, $length);
+  return strtoupper($randomletter);
+}
+
+function stampagiocatore() {
+
+  $giocatore = [
+
+    'Codice' => generaLettere().creacodice(),
+    'Punti'=>  generaRandom(),
+    'Rimbalzi'=> generaRandom(),
+    'Falli'=> generaRandom(),
+    'Tiri da 2 punti' => generaRandom(),
+    'Tiri da 3 punti' => generaRandom()
+
+  ];
+
+  foreach ($giocatore as $key => $value) {
+    echo $key.':'.' '.$value.'<br>';
+  }
+
+}
+
+
+ ?>
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+include 'data.php';
+ ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Php basket</title>
+    <link rel="stylesheet" href="public/css/app.css">
+  </head>
+  <body>
+
+    <?php
+
+    print_r(stampagiocatore());
+
+     ?>
+
+  </body>
+</html>
